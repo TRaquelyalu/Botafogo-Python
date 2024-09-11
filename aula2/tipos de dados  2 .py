3b80846ee410ee4f302d080a06e95cb294350d72
TRaquelyalu <raquelsouza170886@gmail.com>
2024-09-11 10:41:37 -0300

    Add files via upload

---
+++ aula2/tipos de dados  2 .py
@@ -0,0 +1,8 @@
+meu_texto = "senac Botafogo"
+print (meu_texto)
+
+#podemos juntar,  o que chamamos de concatenar
+print(f'estou estudando no {meu_texto} agora')
+print (meu texto.upper()) # MAIÚSCULO
+PRINT(meu_texto.LOWER()) # maiúsculo
+print(meu_texto.capitalize()) # primeira letra em maiúsculo
